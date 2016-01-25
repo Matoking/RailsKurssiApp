aed97943249c8be4797129d45435faba05adc24e
Janne Pulkkinen <jannepulk@gmail.com>
2016-01-25 15:41:23 +0200

    Everything up to task 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,14 @@
 class Beer < ActiveRecord::Base
     belongs_to :brewery
+    has_many :ratings
+
+    def average_rating
+        if ratings.empty?
+            return nil
+        else
+            sum = 0
+            ratings.each { |rating| sum += rating.score }
+            return sum.to_f / ratings.count.to_f
+        end
+    end
 end
