ff05976d0dc502603e1939977eb7c99754f1796c
Janne Pulkkinen <jannepulk@gmail.com>
2016-02-02 11:39:12 +0200

    Tasks up to 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
 
   resources :beers
   resources :breweries
+  resources :users
 
   resources :ratings, only: [:index, :new, :create, :destroy]
+
+  get 'signup', to: 'users#new'
+
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+
+  resource :session, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
